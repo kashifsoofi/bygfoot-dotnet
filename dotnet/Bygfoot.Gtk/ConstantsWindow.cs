19794e8d1766444344274070abc7bbc866993cf9
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2021-03-23 11:03:04 +0000

    Extracted Constants and Options Windows

---
+++ dotnet/Bygfoot.Gtk/ConstantsWindow.cs
@@ -0,0 +1,28 @@
+namespace Bygfoot
+{
+    using System;
+    using Gtk;
+    using UI = Gtk.Builder.ObjectAttribute;
+
+    class ConstantsWindow : Window
+    {
+        public ConstantsWindow() : this(new Builder("ConstantsWindow.glade")) { }
+
+        private ConstantsWindow(Builder builder) : base(builder.GetObject("window_constants").Handle)
+        {
+            builder.Autoconnect(this);
+        }
+
+        private void on_window_constants_destroy_event(object sender, DestroyEventArgs a)
+        {
+        }
+
+        private void on_window_constants_delete_event(object sender, DeleteEventArgs a)
+        {
+        }
+
+        private void on_button_constants_save_clicked(object sender, EventArgs a)
+        {
+        }
+    }
+}
