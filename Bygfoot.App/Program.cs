4d80f8ca1cbb65a07892abf7d183b7a9826e9f19
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-01-14 22:46:27 +0000

    Output libbygfoot.so to bin directory, call c method from dotnet

--- Bygfoot.App/Program.cs
+++ Bygfoot.App/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+public class Program
+{
+    [DllImport("libbygfoot.so", CharSet = CharSet.Unicode)]
+    private static extern int c_main(int argc, string[] argv);
+
+    public static void Main(string[] args)
+    {
+        c_main(args.Length, args);
+    }
+}
