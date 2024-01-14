b65d6072cb20d2be16673ce7a2b0b01cf987cffe
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-01-14 21:29:01 +0000

    Added domain objects and console application

---
+++ Bygfoot.Domain/NameList.cs
@@ -0,0 +1,12 @@
+﻿namespace Bygfoot.Domain;
+
+/** A list of first names and last names
+  * from a file. */
+public class NameList
+{
+    /** The file id (the part between 'player_names_' and '.xml'). */
+    public string sid;
+    /** Arrays of strings holding the names. */
+    public List<string> firstNames;
+    public List<string> lastNames;
+}
