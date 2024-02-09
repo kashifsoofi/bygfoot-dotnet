effe537847faaaf6b0836f334727dd9d1ac86202
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-02-09 00:47:57 +0000

    Show fist level

--- Bygfoot.Gtk/Windows/StartupWindow.cs
+++ Bygfoot.Gtk/Windows/StartupWindow.cs
@@ -1,4 +1,5 @@
 using Bygfoot.Store;
+using Gio;
 using Gtk;
 using WindowHandle = Gtk.Internal.WindowHandle;
 
@@ -6,6 +7,7 @@ namespace Bygfoot.Gtk.Windows;
 
 public class StartupWindow : Window
 {
+    [Connect("combo_country")] private DropDown? _comboCountry; 
     [Connect("button_team_selection_back")] private Button _buttonBack;
     [Connect("team_selection_cancel")] private Button _buttonCancel;
 
@@ -19,11 +21,79 @@ public class StartupWindow : Window
         App = app;
         _buttonBack!.OnClicked += OnBackClicked;
         _buttonCancel!.OnClicked += OnCancelClicked;
+
+        ShowCountries();
     }
 
     public StartupWindow(App app)
         : this(new Builder("startup.ui"), app)
     { }
+
+    private void ShowCountries()
+    {
+        var countryList = new Dictionary<string, string[]>
+        {
+            ["asia"] = ["china", "japan"],
+            ["europe"] = ["england", "france", "scotland"],
+            ["south america"] = ["argentina", "brazil", "chile"],
+        };
+
+        var rootList = StringList.New(countryList.Keys.ToArray());
+
+        var treeListModel = TreeListModel.New(rootList, false, false, createModel);
+        var selectionModel = SingleSelection.New(treeListModel);
+        _comboCountry!.SetModel(treeListModel);
+
+        var factory = SignalListItemFactory.New();
+        factory.OnSetup += (_, args) => {
+            var label = Label.New("");
+            label.Halign = Align.Start;
+            label.CanTarget = false;
+
+            var expander = TreeExpander.New();
+            expander.Child = label;
+
+            ((ListItem)args.Object).Child = expander;
+        };
+        factory.OnBind += (sender, args) => {
+            var listItem = (ListItem)args.Object;            
+            var pos = listItem.Position;
+
+            var expander = listItem.Child as TreeExpander;
+            var label = expander!.Child as Label;
+
+            var value = rootList.GetString(pos);
+            label!.SetMarkup(value);
+
+            var treeListRow = treeListModel.GetChildRow(pos);
+            expander.ListRow = treeListRow;
+        };
+        _comboCountry.Factory = factory;
+    }
+
+    private ListModel? createModel(GObject.Object item)
+    {
+        var countryList = new Dictionary<string, string[]>
+        {
+            ["asia"] = ["china", "japan"],
+            ["europe"] = ["england", "france", "scotland"],
+            ["south america"] = ["argentina", "brazil", "chile"],
+        };
+        var stringObject = item as StringObject;
+        if (stringObject is null)
+        {
+            return null;
+        }
+
+        var key = stringObject.String;
+        if (!countryList.ContainsKey(key))
+        {
+            return null;
+        }
+
+        var childList = StringList.New(countryList[key]);
+        return TreeListModel.New(childList, false, true, createModel);
+    }
 
     private void OnBackClicked(Button sender, EventArgs args)
     {
